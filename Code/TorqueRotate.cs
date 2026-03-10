2e83d6383da2ce59f674917444b78927a0620002
chorueang-rose <iamrobinxz@gmail.com>
2026-03-10 10:59:52 +0700

    Add files via upload

---
+++ Code/TorqueRotate.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueRotate : MonoBehaviour
+{
+    public float torqueRotate = 5f;
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(0, 0, torqueRotate);
+        }
+    }
+}
